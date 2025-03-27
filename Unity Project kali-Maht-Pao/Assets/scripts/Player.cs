989a36ad265dc2ceabd0caea571b47d762321564
kali_L <kaliyanalimear@gmail.com>
2025-03-27 10:33:45 -0400

    Update Player.cs

--- Unity Project kali-Maht-Pao/Assets/scripts/Player.cs
+++ Unity Project kali-Maht-Pao/Assets/scripts/Player.cs
@@ -1,64 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Security.Cryptography;
-using System.Threading;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     //how to define a variable
-    //1. access modifier public or private
-    //2. date type: int, float, bool. string
+    //1. access modifier: public or private
+    //2. data type: int, float, bool, string
     //3. variable name: camelCase
     //4. value: optional
 
-    public float playerSpeed = 6f;
+    private float playerSpeed;
     private float horizontalInput;
     private float verticalInput;
 
-    private float horizontalScreenLimit =
-    private float verticalScreenLimit =
-        public GameObject bulletPrefab; 
+    private float horizontalScreenLimit = 9.5f;
+    private float verticalScreenLimit = 6.5f;
+
+    public GameObject bulletPrefab;
 
     void Start()
     {
-        //This function is called a the start of the game
-       
-    void update()
-    {
-        //This function is called every frame; 60 frames/second
-        Movement()
-        shooting()
-
+        playerSpeed = 6f;
+        //This function is called at the start of the game
 
     }
-        void shootting()
 
+    void Update()
+    {
+        //This function is called every frame; 60 frames/second
+        Movement();
+        Shooting();
+
+    }
+
+    void Shooting()
+    {
+        //if the player presses the SPACE key, create a projectile
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            //if the player presses the SPACE key, create a projectile
-            if(horizontalInput.GetKeyDown(KeyCode.space))
+            Instantiate(bulletPrefab, transform.position + new Vector3(0, 1, 0), Quaternion.identity);
         }
+    }
 
     void Movement()
     {
-            //Read the input from the player
-            horizontalInput = Input.GetAxis("Horizonttal");
+        //Read the input from the player
+        horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
-        //Move the Player
-         Transform.Translate(new Vector3(horizontalInput, 
-             verticalInput, 0) *Time.deltaTime * playerspeed); 
-
+        //Move the player
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * Time.deltaTime * playerSpeed);
         //Player leaves the screen horizontally
-        if(Transform.position.x > horizontalScreenLimit || transform.position.x < -=horizontalScreenLimit)
-            {
-                 transform.position = new 
-            }
-   
-
-    {
-    
-   
-}
-
-
+        if (transform.position.x > horizontalScreenLimit || transform.position.x <= -horizontalScreenLimit)
+        {
+            transform.position = new Vector3(transform.position.x * -1, transform.position.y, 0);
+        }
+        //Player leaves the screen vertically
+        if (transform.position.y > verticalScreenLimit || transform.position.y <= -verticalScreenLimit)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y * -1, 0);
+        }
+    }
